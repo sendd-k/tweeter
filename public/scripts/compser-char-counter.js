3d8a6405202d4e6af185b874bb6daabfedee55c1
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-24 22:29:47 +0000

    adding a character counter

---
+++ public/scripts/compser-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#tweetText").on("keyup", function () {
+    let maxLength = 140;
+    const currentLength = $(this).val().length  
+    console.log(currentLength)
+    let remainder = maxLength - currentLength
+    $(".counter").text(remainder)
+    if(currentLength > maxLength) {
+      $(".counter").css("color", "red")
+    } else {
+      $(".counter").css("color", "black")
+    }
+  })
+});

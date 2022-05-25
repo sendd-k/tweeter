003398f81206b9c9a2f819f22deb8800c982fb80
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-25 21:08:11 +0000

    adding createtweet function with tweet layout

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,42 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data =   {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1653276720846
+}
 
+
+const createTweetElement = function(tweet) {
+  const $tweet = $(`<article class="tweetBody">
+  <header>
+    <div class="tweetHeader">
+      <div class="grid-child imgName">
+    <img class="userIMG" src=${data.user.avatars}>
+    <p class="userName">${data.user.name}</p>
+  </div>
+  <div class="grid-child">
+    <span class="userHandle">${data.user.handle}</span>
+    </div>
+  </div>
+  </header>
+  <p class="tweetContent">${data.content.text}</p>
+  <footer class="tweetFooter">
+    <span class="tweetDate">${data.created_at}</span>
+    <span class="tweetShare">
+      <i class="fa-solid fa-flag fa-2xs"></i>
+      <i class="fa-solid fa-retweet fa-2xs"></i>
+      <i class="fa-solid fa-heart fa-2xs"></i>
+    </span>
+  </footer>
+</article>`)
+
+  return $tweet;
+};
